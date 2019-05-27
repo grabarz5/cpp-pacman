9f46518c58cd276c2cfd762bcd2ad9b6e80f603f
Paweł Grabowski <pawel.grabowski@miscof.it>
2019-05-27 08:29:40 +0200

    Przeniesiono konfigurację konsoli do oddzielnego pliku. Nie działa!

---
+++ config.cpp
@@ -0,0 +1,28 @@
+#include <windows.h>
+
+void consoleConfig() {
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = 0;
+	cfi.dwFontSize.X = 32;                   // Width of each character in the font
+	cfi.dwFontSize.Y = 32;                  // Height
+	cfi.FontFamily = FF_DONTCARE;
+	cfi.FontWeight = FW_NORMAL;
+
+	wcscpy_s(cfi.FaceName, L"Consolas"); // Choose your font
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+
+	HWND console = GetConsoleWindow();
+	RECT ConsoleRect;
+	GetWindowRect(console, &ConsoleRect);
+	MoveWindow(console, ConsoleRect.left, ConsoleRect.top, 712, 900, TRUE);
+
+	//Hide cursor
+	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_CURSOR_INFO     cursorInfo;
+
+	GetConsoleCursorInfo(out, &cursorInfo);
+	cursorInfo.bVisible = false; // set the cursor visibility
+	SetConsoleCursorInfo(out, &cursorInfo);
+}
--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
-﻿#include "config.h"
-#include <iostream>
+﻿#include <iostream>
 #include <windows.h>
 #include <stdio.h>
 #include <conio.h>
 
-#define CLOCK 128
+#define CLOCK 250
 
 using namespace std;
 
 //deklaracje funkcji
-//void consoleConfig();
+void consoleConfig();
 void draw(int x, int y);
 void moveUp();
 void moveDown();
@@ -192,4 +191,31 @@ void draw(int x, int y) {
 		}
 		cout << endl;
 	}
+}
+
+void consoleConfig() {
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = 0;
+	cfi.dwFontSize.X = 32;                   // Width of each character in the font
+	cfi.dwFontSize.Y = 32;                  // Height
+	cfi.FontFamily = FF_DONTCARE;
+	cfi.FontWeight = FW_NORMAL;
+
+	wcscpy_s(cfi.FaceName, L"Consolas"); // Choose your font
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+
+	HWND console = GetConsoleWindow();
+	RECT ConsoleRect;
+	GetWindowRect(console, &ConsoleRect);
+	MoveWindow(console, ConsoleRect.left, ConsoleRect.top, 712, 900, TRUE);
+
+	//Hide cursor
+	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_CURSOR_INFO     cursorInfo;
+
+	GetConsoleCursorInfo(out, &cursorInfo);
+	cursorInfo.bVisible = false; // set the cursor visibility
+	SetConsoleCursorInfo(out, &cursorInfo);
 }
